fa8414008051f7b39c1812e0f2bddd89489ac961
Yaolin Ge <geyaolin@gmail.com>
2024-04-27 20:13:25 +0200

    update new front page

---
+++ presentation/js/user_defined.js
@@ -0,0 +1,13 @@
+// var myDiv = document.querySelector('#ocean');
+
+// var observer = new MutationObserver(function(mutations) {
+//     mutations.forEach(function(mutation) {
+//         if (myDiv.classList.contains('earth') && myDiv.classList.contains('step') && myDiv.classList.contains('prev')) {
+//             myDiv.style.opacity = 0.5;
+//         }
+//     });
+// });
+
+// var config = { attributes: true, childList: true, characterData: true };
+
+// observer.observe(myDiv, config);
---
+++ presentation/script.md
@@ -0,0 +1,6 @@
+Ph.D. Presentation Script
+
+Start with earth view
+Move to ocean importance
+Move to ocean sampling
+Move to ocean front
