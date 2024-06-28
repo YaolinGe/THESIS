aa6b05713427306d7621f465389ad3d1c9a33600
Yaolin Ge <geyaolin@gmail.com>
2024-06-28 21:09:22 +0200

    update repo structure for deployment

--- src/apps/Components/Act.py
+++ src/apps/Components/Act.py
@@ -1,2 +1,19 @@
-def renderActPage(): 
-    pass
+import os
+import streamlit as st
+from PIL import Image
+
+def renderActPage():
+    st.title("Act")
+    
+    figpath = os.path.join(os.getcwd(), "src", "apps", "figs")
+    image1 = Image.open(os.path.join(figpath, "act.webp"))
+    image2 = Image.open(os.path.join(figpath, "ros_imc_bridge.png"))
+    
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image(image1, use_column_width=True)
+    with col2:
+        st.image(image2, use_column_width=True)
+
+if __name__ == "__main__":
+    renderActPage()
